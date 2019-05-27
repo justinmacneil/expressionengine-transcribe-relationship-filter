ca55e7ea94f1cc559adc3b1576ca6d047f210e74
Justin MacNeil <jmacneil@realdecoy.com>
2019-05-27 13:08:26 -0400

    language clean-up

--- transcribe_relationship_filter/language/english/transcribe_relationship_filter_lang.php
+++ transcribe_relationship_filter/language/english/transcribe_relationship_filter_lang.php
@@ -2,5 +2,5 @@
 $lang = array(
 	'mode_include_exclude' => 'Mode: Include or exclude',
 	'include_field_ids' => 'Filter relationship field IDs by entry language (comma-separated; use * to filter all fields).',
-	'exclude_field_ids' => 'Exclude relationship field IDs by entry language (comma-separated).',
+	'exclude_field_ids' => 'Exclude relationship field IDs from filter (comma-separated).',
 );
